672b3d0f1813a59af86e15bd6962c745ae86e509
aibaozi69 <fopdoodle7532@gmail.com>
2023-07-13 11:57:55 +0800

    初始化 Spring Boot Project

---
+++ sprin-gmall/src/main/java/com/aibaozi/springmall/SprinGmallApplication.java
@@ -0,0 +1,13 @@
+package com.aibaozi.springmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinGmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinGmallApplication.class, args);
+    }
+
+}
---
+++ sprin-gmall/src/test/java/com/aibaozi/springmall/SprinGmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aibaozi.springmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinGmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
