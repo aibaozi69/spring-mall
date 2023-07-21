a86ecff21c3b4a116b25c37a8e73efe357549bf8
aibaozi69 <fopdoodle7532@gmail.com>
2023-07-21 10:09:12 +0800

    商品功能-實作查詢商品列表(基礎實作)

--- sprin-gmall/src/main/java/com/aibaozi/springmall/dao/ProductDao.java
+++ sprin-gmall/src/main/java/com/aibaozi/springmall/dao/ProductDao.java
@@ -3,7 +3,12 @@ package com.aibaozi.springmall.dao;
 import com.aibaozi.springmall.dto.ProductRequest;
 import com.aibaozi.springmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product>getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- sprin-gmall/src/main/java/com/aibaozi/springmall/service/ProductService.java
+++ sprin-gmall/src/main/java/com/aibaozi/springmall/service/ProductService.java
@@ -3,7 +3,12 @@ package com.aibaozi.springmall.service;
 import com.aibaozi.springmall.dto.ProductRequest;
 import com.aibaozi.springmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product>getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
